bbab1943de4ad4d6195917148809df3e98e846ac
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-02 13:36:32 +0100

    update translate.py

--- translate.py
+++ translate.py
@@ -1,10 +1,17 @@
-import sounddevice as sd
+import pulsectl
 
-def list_audio_devices():
-    devices = sd.query_devices()
-    for i, device in enumerate(devices):
-        print (f'{device}')
-        # print(f"Device {i}: {device['name']} - {device['max_input_channels']} input channels, {device['max_output_channels']} output channels")
+def list_pulse_audio_devices():
+    pulse = pulsectl.Pulse('device-list')
+    devices = pulse.sink_list() + pulse.source_list()  # Sinks are output devices, sources are input devices
+    for device in devices:
+        print(f"Device Name: {device.name}")
+        print(f"Description: {device.description}")
+        print(f"Index: {device.index}")
+        print(f"Sample Rate: {device.sample_rate}")
+        print(f"Channels: {device.channel_count}")
+        print(f"Volume: {device.volume.values}")
+        print(f"Muted: {device.mute}")
+        print()
 
 if __name__ == "__main__":
-    list_audio_devices()
+    list_pulse_audio_devices()

7ce0bc6c169aba7276a019285380e89ede1157d6
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-02 13:41:50 +0100

    update translate.py

--- translate.py
+++ translate.py
@@ -3,6 +3,10 @@ import pulsectl
 def list_pulse_audio_devices():
     pulse = pulsectl.Pulse('device-list')
     devices = pulse.sink_list() + pulse.source_list()  # Sinks are output devices, sources are input devices
+
+    print (f'{dir(devices[0])}')
+
+    '''
     for device in devices:
         print(f"Device Name: {device.name}")
         print(f"Description: {device.description}")
@@ -12,6 +16,7 @@ def list_pulse_audio_devices():
         print(f"Volume: {device.volume.values}")
         print(f"Muted: {device.mute}")
         print()
+    '''
 
 if __name__ == "__main__":
     list_pulse_audio_devices()
